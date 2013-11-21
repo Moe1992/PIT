dd0aaf3b0babb6432b3b3599b02d18e37f42f2c9
Feragho <holger.digel@gmx.de>
2013-11-21 18:10:41 +0100

    Laufzeitanalyse erstellt und angefangen

---
+++ PraktikumIT/PraktikumIT/LaufzeitAnalysator.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Faktoren.h"
+#include "ListenElement.h"
+#include <map>
+
+class LaufzeitAnalysator
+{
+public:
+	LaufzeitAnalysator(void);
+	~LaufzeitAnalysator(void);
+
+private:
+	Faktoren* faktoren;
+	ListenElement* startElement;
+	long frequenz;
+	std::string uebergangspfad;
+	std::string ausgangspfad;
+	double laufzeitUebergangspfad;
+	double laufzeitAusgangspfad;
+	struct DFS_Daten
+	{
+		SchaltwerkElement* VaterElement;
+		double PfadLaufzeit;
+	};
+	std::map<SchaltwerkElement*, DFS_Daten> DFS_Zwischenspeicher;
+	void EinzelLaufzeit(); //Berechnung der Laufzeit der einzelnen Gatter
+
+};
+
